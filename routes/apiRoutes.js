75a2133642bfcdcfc869b7cae2021de9c23f374b
Veynitha Bandara <veynithabandara@gmail.com>
2024-12-05 02:26:05 +0530

    Controller layer added to external API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,50 +1,9 @@
 const express = require("express");
-const {
-  forwardVesselDataToHotspot,
-  forwardVesselDataToWebServer,
-} = require("../services/apiService");
+const { reRouteGpsData } = require("../controllers/apiController");
 
 const router = express.Router();
 
-//Data coming from the gateway is Re routed to Aqua Safe
-router.post("/reRoute/Gps", async (req, res) => {
-  const { id, l, s } = req.body;
-
-  if (!id || !l || typeof s !== "number") {
-    return res.status(400).json({
-      success: false,
-      message: 'Invalid data format. Ensure "id", "l", and "s" are provided.',
-    });
-  }
-
-  try {
-    let formattedData;
-    let apiResponse;
-
-    if (s === 0) {
-      // If not SOS data, the data is sent to the hotspot without the s
-      formattedData = { id, l };
-      apiResponse = await forwardVesselDataToHotspot(formattedData);
-    } else {
-      //If SOS the data is sent to aqua safe with s
-      formattedData = { id, l, s };
-      apiResponse = await forwardVesselDataToWebServer(formattedData);
-    }
-
-    console.log(apiResponse);
-
-    res.status(200).json({
-      success: true,
-      message: "Data successfully sent to the API.",
-      apiResponse,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to send data to the API.",
-      error: error.message,
-    });
-  }
-});
+// Data coming from the gateway is rerouted to Aqua Safe
+router.post("/reRoute/Gps", reRouteGpsData);
 
 module.exports = router;
